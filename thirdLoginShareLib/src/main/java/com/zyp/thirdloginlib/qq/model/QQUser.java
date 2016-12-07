e72be74f97e41b37bcde18ec50eb9aceaeff59c5
jox <joxpeng@163.com>
2016-12-07 18:12:15 +0800

    修改bug

--- thirdLoginShareLib/src/main/java/com/zyp/thirdloginlib/qq/model/QQUser.java
+++ thirdLoginShareLib/src/main/java/com/zyp/thirdloginlib/qq/model/QQUser.java
@@ -25,7 +25,6 @@ public class QQUser extends AccountResult {
     private String yellow_vip_level;
     private String level;
     private String is_yellow_year_vip;
-    private
 
     public int getRet() {
         return ret;
